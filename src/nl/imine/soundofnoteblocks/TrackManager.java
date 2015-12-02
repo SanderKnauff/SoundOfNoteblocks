54775be0205b29484dcec43b28466908ac9ffbca
MakerTim <makertim@outlook.com>
2015-12-02 23:52:12 +0100

    just pretty TrackManager a bit
    (im sorry)

--- src/nl/imine/soundofnoteblocks/TrackManager.java
+++ src/nl/imine/soundofnoteblocks/TrackManager.java
@@ -1,12 +1,12 @@
 package nl.imine.soundofnoteblocks;
 
-import com.google.gson.Gson;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import com.google.gson.Gson;
 
 /**
  *
@@ -14,37 +14,36 @@ import java.util.List;
  */
 public class TrackManager {
 
-    public static final String TRACK_REPO_DIR = "/home/webserver/files/iMineNetwork/NBS/".replaceAll("///", File.separator);
+    public static final String TRACK_REPO_DIR = "/home/webserver/files/iMineNetwork/NBS/";
     public static final String NBS_EXSTENTION = ".nbs";
     public static final String JSON_EXSTENTION = ".json";
 
     private List<Track> trackList = new ArrayList<>();
 
     public TrackManager() {
-        trackList = loadTracks();
+        loadTracks();
     }
 
-    private List<Track> loadTracks() {
-        List<Track> ret = new ArrayList<>();
+    private void loadTracks() {
         try {
             Gson gson = new Gson();
-            Track[] tracks = gson.fromJson(new FileReader(TRACK_REPO_DIR + "trackList" + JSON_EXSTENTION), Track[].class);
-            ret = Arrays.asList(tracks);
+            Track[] tracks = gson.fromJson(new FileReader(TRACK_REPO_DIR + "trackList" + JSON_EXSTENTION),
+                    Track[].class);
+            trackList = Arrays.asList(tracks);
         } catch (FileNotFoundException fnfe) {
             System.out.println("FileNotFoundException: " + fnfe.getMessage());
         }
-        return ret;
     }
-    
-    public void setTracks(List<Track> trackList){
+
+    public void setTracks(List<Track> trackList) {
         this.trackList = trackList;
     }
-    
-    public List<Track> getTracks(){
+
+    public List<Track> getTracks() {
         return trackList;
     }
-    
-    public void reloadTracks(){
-        trackList = loadTracks();
+
+    public void reloadTracks() {
+        loadTracks();
     }
 }
