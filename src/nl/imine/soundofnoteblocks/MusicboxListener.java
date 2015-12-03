68c4a001b0f221f73854833df68611b7cf7cd952
MakerTim <makertim@outlook.com>
2015-12-03 13:54:45 +0100

    testing if item can be null

--- src/nl/imine/soundofnoteblocks/MusicboxListener.java
+++ src/nl/imine/soundofnoteblocks/MusicboxListener.java
@@ -30,6 +30,7 @@ public class MusicboxListener implements Listener {
             if (evt.getClickedBlock().getType().equals(Material.JUKEBOX)) {
                 if (!(((org.bukkit.block.Jukebox) evt.getClickedBlock().getState()).isPlaying())) {
                     if (evt.getPlayer().hasPermission("imine.uhc.vip")) {
+                        System.out.println(evt.getItem());
                         //if (evt.getItem() == null && !(evt.getItem().getType().name().toLowerCase().contains("record"))) {
                             Musicbox jukebox = Musicbox.findJukebox(evt.getClickedBlock().getLocation());
                             Container c = GuiManager.getInstance().createContainer("Choose Track", 45);
