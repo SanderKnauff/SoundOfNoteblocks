66a8ca22c1d4c99b15dd35b7b721abefbf277eb2
MakerTim <MakerTim@outlook.com>
2016-05-06 04:12:41 +0200

    Safe currunt musicplayers

--- src/main/java/nl/imine/soundofnoteblocks/SoundOfNoteBlocksPlugin.java
+++ src/main/java/nl/imine/soundofnoteblocks/SoundOfNoteBlocksPlugin.java
@@ -50,6 +50,7 @@ public class SoundOfNoteBlocksPlugin extends JavaPlugin implements Listener {
 				tempFile.deleteOnExit();
 			}
 		}
+		MusicPlayerManager.safe();
 	}
 
 	private void setupConfig() {
